8bc62bdaa5c486db4b7e4eaeb1eb7295e7c52da2
Le Minh Giang <mgiang2015@gmail.com>
2021-06-03 18:38:34 +0800

    Set up skeleton layout

--- reddit-inspired-frontend/src/App.js
+++ reddit-inspired-frontend/src/App.js
@@ -1,6 +1,108 @@
-import logo from './logo.svg';
+// react-datetime library to implement time
+// import Datetime from 'react-datetime'; // a bit buggy now
+import logo from './resources/images/logo200x200.png';
 import './App.css';
+import React from 'react';
 
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+    </div>
+  );
+}
+
+
+class Header extends React.Component {
+  render() {
+    return (
+    <div className="header">
+      <img src={logo} alt="Logo"/>
+      <h1><i>/r/Dance: Where dancers unite</i></h1>
+    </div>
+    );
+  }
+}
+
+class MainContent extends React.Component {
+  render() {
+    return (
+    <div className="main-container">
+      <ThreadContainer />
+      <SideBar />
+    </div>
+    );
+  }
+}
+
+class ThreadContainer extends React.Component {
+  render() {
+    return (
+    <div className="thread-container">
+      <Thread />
+      <Thread />
+      <Thread />
+      <Thread />
+    </div>
+    );
+  }
+}
+
+class SideBar extends React.Component {
+  render() {
+    return (
+    <div class="sidebar">
+      <h2>This is sidebar</h2>
+    </div>
+    );
+  }
+}
+
+class Thread extends React.Component {  
+  render() {
+    const dateString = "3 Jun 2021"; // temporary hard code
+    return (
+    <div className="thread">
+      <PostInformation className="thread-information" author="kelele" date={dateString}/>
+      <h2>Mock Thread Heading</h2>
+    </div>
+    );
+  }
+}
+
+class PostInformation extends React.Component {
+  render() {
+    const infoString = "Posted by u/" + this.props.author + " on " + this.props.date;
+    return (
+    <div>
+      <h6>{infoString}</h6>
+    </div>
+    );
+  }
+}
+
+/*
+class NavBar extends React.Component {
+
+}
+
+*/
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +123,5 @@ function App() {
     </div>
   );
 }
+*/
 
-export default App;
