7b0fb9fdb6f89f5bec2714c639042c9e08557ab9
Le Minh Giang <mgiang2015@gmail.com>
2021-06-04 17:00:57 +0800

    Fix header to match content

--- reddit-inspired-frontend/src/components/Header.js
+++ reddit-inspired-frontend/src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from '../resources/images/logo200x200.png';
+import logo from '../resources/images/dota2.png';
 
 class Header extends React.Component {
   render() {
     return (
     <div className="header">
-      <img src={logo} alt="Logo"/>
-      <h1><i>/r/Dance: Where dancers unite</i></h1>
+      <img class="logo" src={logo} alt="Logo"/>
+      <h1><i>/r/DotA2</i></h1>
     </div>
     );
   }
